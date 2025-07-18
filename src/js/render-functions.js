74dfe5d5530dd9da9c8a2383ca29e214da20eda8
Elvira Antoniuk <elviraantonuk5@gmail.com>
2025-07-18 18:29:17 +0100

    tasks

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,2 +1,44 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+// бібліотека
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// розмітка
+function createGalleryMarkup(images) {
+  return images
+    .map(image => {
+    return `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
+        </div>
+      </li>
+    `;
+  }).join('');
+}
+
+// зображення
+export function renderImages(images) {
+  const galleryEl = document.querrySelector('.gallery');
+  galleryEl.innerHTML = createGalleryMarkup(images);
+  lightbox.refresh();
+}
+
+//loader
+export function showLoader() {
+  document.getElementById('loader').classList.remove('hidden');
+}
+
+export function hideLoader() {
+  document.getElementById('loader').classList.add('hidden');
+}
